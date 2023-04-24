2493de4182142c719fcb37bc7b488c55b4a4ce8e
ColeHunt <huntcr2001@gmail.com>
2023-04-24 14:20:05 -0500

    Adds packet header and keyboard capture

--- operatorInterface.py
+++ operatorInterface.py
@@ -17,9 +17,14 @@ joystick.init()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((TCP_IP, TCP_PORT))
 
+# Packet Header Command Types
+CMD = 0
+CONTROL = 1
+
 # Loop to continuously send Xbox controller data
 while True:
     pygame.event.pump()
+    keys=pygame.key.get_pressed()
 
     # Get controller input values
     axis_x = joystick.get_axis(0)
@@ -29,11 +34,21 @@ while True:
 
     # Convert input values to bytes
     data = bytearray()
+    data.append(CMD)
     data.append(int(axis_x * 127 + 127))
     data.append(int(axis_y * 127 + 127))
     data.append(button_a)
     data.append(button_b)
 
+    # Disable Robot on spacebar press
+    if keys[pygame.K_SPACE]:
+        data = bytearray(CONTROL, 0)
+    if keys[pygame.K_BREAK]:
+        data = bytearray(CONTROL, 1)
+
     # Send data over TCP socket
+    print(data)
     sock.sendall(data)
     time.sleep(0.05)
+
+
