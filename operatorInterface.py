746bee03457e3e6762af2fec65c9af7728846e06
Dustin Fouts <144202544+DustinFouts14@users.noreply.github.com>
2023-10-05 14:08:09 -0500

    Update operatorInterface.py

--- operatorInterface.py
+++ operatorInterface.py
@@ -26,11 +26,15 @@ while True:
     l_axis_y = joystick.get_axis(1)
     r_axis_x = joystick.get_axis(3)
     r_axis_y = joystick.get_axis(4)
+    LeftTrigger = joystick.get_axis(2)
+    RightTrigger = joystick.get_axis(5)
     button_a = joystick.get_button(0)
     button_b = joystick.get_button(1)
     button_x = joystick.get_button(2)
     button_y = joystick.get_button(3)
     button_start = joystick.get_button(7)
+    LeftBumper = joystick.get_button(4)
+    RightBumper = joystick.get_button(5)
 
     # Convert input values to bytes
     data = bytearray()
@@ -38,11 +42,15 @@ while True:
     data.append(int(l_axis_y * 127 + 127))
     data.append(int(r_axis_x * 127 + 127))
     data.append(int(r_axis_y * 127 + 127))
+    data.append(int(LeftTrigger * 127 + 127))
+    data.append(int(RightTrigger * 127 + 127))
     data.append(button_a)
     data.append(button_b)
     data.append(button_x)
     data.append(button_y)
     data.append(button_start)
+    data.append(LeftBumper)
+    data.append(RightBumper)
 
     # Send data over TCP socket
     sock.sendall(data)
