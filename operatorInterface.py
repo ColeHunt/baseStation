a898523b4e7b6871d7fafecec90f721ad3f28560
idenning2003 <idenning@iastate.edu>
2023-11-16 19:40:01 -0600

    Added all joystick values to send to Jetson

--- operatorInterface.py
+++ operatorInterface.py
@@ -14,7 +14,7 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 # Create a TCP socket and connect to the server
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 sock.connect((TCP_IP, TCP_PORT))
 
 # Loop to continuously send Xbox controller data
@@ -22,49 +22,53 @@ while True:
     pygame.event.pump()
 
     # Get controller input values
-    l_axis_x = joystick.get_axis(0) #0
-    l_axis_y = joystick.get_axis(1) #1
-    r_axis_x = joystick.get_axis(3) #2
-    r_axis_y = joystick.get_axis(4) #3
-    button_a = joystick.get_button(0) #4
-    button_b = joystick.get_button(1) #5
-    button_x = joystick.get_button(2) #6
-    button_y = joystick.get_button(3) #7
-    button_start = joystick.get_button(7) #8
-    leftBumper = joystick.get_button(4) #9
-    rightBumper = joystick.get_button(5) #10
-    leftTrigger = joystick.get_axis(2) #11
-    rightTrigger = joystick.get_axis(5) #12
-    # TODO: Add input values
-    leftStick_down = joystick.get_button(9) #13
-    rightStick_down = joystick.get_button(10) #14
-    button_back = joystick.get_button(6) #15
-    button_xbox = joystick.get_button(8) #16
-    d_pad = joystick.get_hat(0) #17
-   
+    l_axis_x = joystick.get_axis(0) # 0
+    l_axis_y = joystick.get_axis(1) # 1
+    leftTrigger = joystick.get_axis(2) # 2
+    rightTrigger = joystick.get_axis(5) # 3
+    button_a = joystick.get_button(0) # 4
+    button_b = joystick.get_button(1) # 5
+    button_x = joystick.get_button(2) # 6
+    button_y = joystick.get_button(3) # 7
+    button_back = joystick.get_button(6) # 8
+    button_start = joystick.get_button(7) # 9
+    button_xbox = joystick.get_button(8) # 10
+    button_lstick = joystick.get_button(9) # 11
+    button_rstick = joystick.get_button(10) # 12
+    leftBumper = joystick.get_button(4) # 13
+    rightBumper = joystick.get_button(5) # 14
+    r_axis_x = joystick.get_axis(3) # 15
+    r_axis_y = joystick.get_axis(4) # 16
+    d_pad_up = joystick.get_hat(0)[1] == 1 # 17
+    d_pad_down = joystick.get_hat(0)[1] == -1 # 18
+    d_pad_left = joystick.get_hat(0)[0] == -1 # 19
+    d_pad_right = joystick.get_hat(0)[0] == 1 # 20
+
 
     # Convert input values to bytes
     data = bytearray()
-    data.append(int(l_axis_x * 127 + 127)) #0
-    data.append(int(l_axis_y * 127 + 127)) #1
-    data.append(int(r_axis_x * 127 + 127)) #2
-    data.append(int(r_axis_y * 127 + 127)) #3
-    data.append(button_a) #4
-    data.append(button_b) #5
-    data.append(button_x) #6
-    data.append(button_y) #7
-    data.append(button_start) #8
-    data.append(leftBumper) #9
-    data.append(rightBumper) #10
-    data.append(int(leftTrigger * 127 + 127)) #11
-    data.append(int(rightTrigger * 127 + 127)) #12
-    data.append(leftStick_down) #13
-    data.append(rightStick_down) #14
-    data.append(button_back) #15
-    data.append(button_xbox) #16
-    data.append(int(d_pad * 127 + 127)) #17
-   
-    # TODO: Add additional controller inputs
+    data.append(int(l_axis_x * 127 + 127)) # 0
+    data.append(int(l_axis_y * -127 + 127)) # 1
+    data.append(int(r_axis_x * 127 + 127)) # 2
+    data.append(int(r_axis_y * -127 + 127)) # 3
+    data.append(int(leftTrigger * 127 + 127)) # 4
+    data.append(int(rightTrigger * 127 + 127)) # 5
+    data.append(button_a) # 6
+    data.append(button_b) # 7
+    data.append(button_x) # 8
+    data.append(button_y) # 9
+    data.append(leftBumper) # 10
+    data.append(rightBumper) # 11
+    data.append(button_back) # 12
+    data.append(button_xbox) # 13
+    data.append(button_start) # 14
+    data.append(button_lstick) # 15
+    data.append(button_rstick) # 16
+    data.append(d_pad_up) # 17
+    data.append(d_pad_down) # 18
+    data.append(d_pad_left) # 19
+    data.append(d_pad_right) # 20
+
 
     # Send data over TCP socket
     sock.sendall(data)
